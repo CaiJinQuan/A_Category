399a9562e9cfc8981cf987e0a7d23b11fb557705
zhangjikuan <jikuan.chang@gmail.com>
2017-12-01 17:23:24 +0800

    添加核心内容

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by zhangjikuan on 2017/12/1.
+//  Copyright © 2017年 zhangjikuan. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
+@interface CTMediator (A)
+- (UIViewController *)A_aViewController;
+@end
